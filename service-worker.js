e3d94f63fb33ffcbce9deb7417075fcc95a238c2
ninemia123 <hagistamatis@protonmail.com>
2026-03-01 11:30:21 +0200

    Update service-worker.js
    δοκιμη

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,18 @@
 const CACHE_NAME = "imerast-cache-v1";
+
+// Χρησιμοποιούμε σχετικές διαδρομές (./) για να παίζει σωστά στο GitHub Pages
 const urlsToCache = [
-  "/imerast/",
-  "/imerast/index.html",
-  
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      // Προσθέτουμε τα αρχεία στο cache
       return cache.addAll(urlsToCache);
     })
   );
@@ -16,8 +21,8 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Επιστρέφει το αρχείο από το cache αν υπάρχει, αλλιώς πάει στο δίκτυο
       return response || fetch(event.request);
     })
   );
 });
-
